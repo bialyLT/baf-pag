25696f921715906a5159f9dd4e555dd34e44a037
liam <liambialy2@gmail.com>
2024-02-05 01:11:56 -0300

    fix some details

--- src/components/nav/DropdownPropiedades.js
+++ src/components/nav/DropdownPropiedades.js
@@ -1,12 +1,12 @@
 export default function DropdownPropiedades({ propiedades }) {
     return (
-        <ul tabIndex={0} className="menu menu-sm dropdown-content lg:menu-horizontal mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-100 flex-nowrap">
+        <ul tabIndex={0} className="menu menu-sm dropdown-content lg:menu-horizontal  z-[1] p-2 shadow bg-base-100 rounded-box w-100 flex-nowrap">
             <li><a href="#inicio">Inicio</a></li>
             <li><a href="#contacto">Contacto</a></li>
             <li>
                     <details>
                         <summary>Propiedades</summary>
-                        <ul className="p-2 lg:menu-sm flex-nowrap">
+                        <ul className="p-2 lg:menu-sm">
                             {propiedades.map((p, i) =>
                                 <li key={i}>
                                     <a href={`#propiedad-${i+1}`}>{p.name}</a>
