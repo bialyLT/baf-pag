714ee58b3c6ea64d1a17644a90c397f79a69b4c0
liam <liambialy2@gmail.com>
2024-02-25 06:10:54 -0300

    probando conexion del backend

--- src/components/nav/DropdownPropiedades.js
+++ src/components/nav/DropdownPropiedades.js
@@ -1,5 +1,19 @@
 import { Link } from 'react-router-dom'
+import { getPublications } from '../../services/apiPublicacions'
+import { useEffect, useState } from 'react'
 const DropdownPropiedades = ({ propiedades }) => {
+  const [publicaciones, setPublicaciones] = useState([])
+
+  useEffect(() => {
+    loadPublicaciones()
+  })
+
+  const loadPublicaciones = () => {
+    getPublications('titleAsc')
+      .then(res => setPublicaciones(res.data.data))
+      .catch(e => console.error('Error al cargar las publicaciones', e))
+  }
+
   return (
     <ul tabIndex={0} className='menu menu-sm dropdown-content lg:menu-horizontal  z-[1] p-2 shadow bg-base-100 rounded-box w-100 flex-nowrap'>
       <li><Link to='/'>Inicio</Link></li>
@@ -8,9 +22,9 @@ const DropdownPropiedades = ({ propiedades }) => {
         <details>
           <summary>Propiedades</summary>
           <ul className='p-2 lg:menu-sm overflow-auto overflow-x-hidden max-h-[500px]'>
-            {propiedades.map((p, i) =>
+            {publicaciones.map((p, i) =>
               <li key={i}>
-                <Link to={`/propiedades/propiedad-${i + 1}/`} state={{ propiedad: p.name, id: i + 1, cantImgs: p.cantImg, isVendido: p.vendido, linkFacebook: p.linkFacebook }} key={i}>{p.name}</Link>
+                <Link to={`/propiedades/propiedad-${i + 1}/`} state={{ propiedad: p.title, id: i + 1, cantImgs: p.images.length, isVendido: p.isVendido, linkFacebook: p.linkFacebook }} key={i}>{p.title}</Link>
               </li>)}
           </ul>
         </details>
---
+++ src/services/apiPublicacions.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000', // URL del backend
+    timeout: 10000, // Tiempo de espera en milisegundos
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+export const getPublications = (orden) => api.get(`/publicaciones?orden=${orden}`)
+export const createPublication = (publicacionData) => api.post('/publicaciones', publicacionData)
+export const updatePublication = (id, publicacionData) => api.put(`/publicaciones/${id}`, publicacionData)
+export const deletePublication = (id) => api.delete(`/publicaciones/${id}`)
