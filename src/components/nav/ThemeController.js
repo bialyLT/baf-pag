319f42ea14c80f604f7078144370cde105f6e8ce
liam <liambialy2@gmail.com>
2024-03-12 18:02:45 -0300

    added theme in localstorage and changed theme controller button

--- src/components/nav/ThemeController.js
+++ src/components/nav/ThemeController.js
@@ -1,22 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from "react"
 
-export default function ThemeController() {
-  const [theme, setTheme] = useState('light')
 
-  const handleChanged = (e) => {
-    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
-  }
+const ThemeController = () => {
+  const [isdark, setIsdark] = useState(
+    JSON.parse(localStorage.getItem('isdark'))
+  )
+
+
 
   useEffect(() => {
-    document.querySelector('html').setAttribute('data-theme', theme)
-    document.querySelector('html').setAttribute('class', theme)
-  }, [theme])
+    localStorage.setItem('isdark', JSON.stringify(isdark))
+    document.querySelector('html').setAttribute('data-theme', isdark ? 'dark' : 'light')
+    document.querySelector('html').setAttribute('class', isdark ? 'dark' : 'light')
+  }, [isdark])
+
 
   return (
-    <label className='flex cursor-pointer gap-2'>
-      <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><circle cx='12' cy='12' r='5' /><path d='M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4' /></svg>
-      <input type='checkbox' value='synthwave' className='toggle theme-controller' onChange={handleChanged} />
-      <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'><path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z' /></svg>
+    <label className="cursor-pointer grid place-items-center ">
+      <input type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" onChange={() => setIsdark(!isdark)} checked={isdark} />
+      <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d=" M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+      <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
     </label>
   )
 }
+
+export default ThemeController
--- src/components/pages/AdminLoginPage.js
+++ src/components/pages/AdminLoginPage.js
@@ -3,17 +3,16 @@ import { TokenContext } from "../../context/tokenAdmin";
 import { useNavigate } from "react-router-dom";
 
 const AdminLoginPage = () => {
-
   const navigate = useNavigate()
   const { getAdminUser, error } = useContext(TokenContext);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
     const data = {
       username: e.target.username.value,
       password: e.target.password.value
     }
-    const user = await getAdminUser(data)
+    const user = getAdminUser(data)
     if (!user) {
       navigate('/admin')
     } else {
@@ -22,25 +21,31 @@ const AdminLoginPage = () => {
   }
 
   return (
-    <section className="max-w-md p-6 mt-32 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-      <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Configuración de administrador</h2>
-      <form method="POST" onSubmit={(e) => handleSubmit(e)}>
-        <div className="grid grid-cols-1 gap-6 mt-4">
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Nombre de usuario</label>
-            <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+    <>
+      <div role="alert" className="alert alert-warning mt-32 max-w-md mx-auto shadow-md">
+        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+        <span>Advertencia: el inicio de sesión esta disponible únicamente para administradores</span>
+      </div>
+      <section className="max-w-md p-6 mt-5 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Configuración de administrador</h2>
+        <form method="POST" onSubmit={(e) => handleSubmit(e)}>
+          <div className="grid grid-cols-1 gap-6 mt-4">
+            <div>
+              <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Nombre de usuario</label>
+              <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+            <div>
+              <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Contraseña</label>
+              <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
           </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Contraseña</label>
-            <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+          <div className="flex justify-center mt-6">
+            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Iniciar sesion</button>
           </div>
-        </div>
-        <div className="flex justify-center mt-6">
-          <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Iniciar sesion</button>
-        </div>
-      </form>
-      {error ? <p>error</p> : <p></p>}
-    </section>
+        </form>
+        {error ? <p>error</p> : <p></p>}
+      </section>
+    </>
   )
 }
 
